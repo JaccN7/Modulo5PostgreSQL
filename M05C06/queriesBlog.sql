3b09bb6707e2860ee07bd982262793387b581ed9
Javiera Cortés <javieracortesceledon@gmail.com>
2022-06-10 19:03:12 -0400

    queriesBlog.sql actualizado

--- M05C06/queriesBlog.sql
+++ M05C06/queriesBlog.sql
@@ -24,6 +24,53 @@ WHERE
 
 -- Listar los usuarios que no han publicado ningún post.
 
+SELECT
+    email AS usuarios_sin_post
+FROM
+    usuario
+    LEFT JOIN post ON usuario.ID = post.usuario_id
+WHERE
+    post.usuario_id IS NULL;
+
 -- Listar todos los post con sus comentarios (incluyendo aquellos que no poseen comentarios).
 
+--Opción 1: Mostrar post y comentarios.
+
+SELECT
+    post.ID,
+    post.titulo,
+    post.usuario_id,
+    CASE
+        WHEN comentario.texto IS NULL THEN 'Sin Comentarios'
+        ELSE comentario.texto
+    END AS comentarios
+FROM
+    post
+    LEFT JOIN comentario ON post.ID = comentario.post_id
+ORDER BY
+    post.ID ASC;
+
+-- Opción 2: Mostrar post y cantidad de comentarios.
+
+SELECT
+    post.ID,
+    post.titulo,
+    post.usuario_id,
+    COUNT (comentario.texto) AS cant_comentarios
+FROM
+    post
+    LEFT JOIN comentario ON post.ID = comentario.post_id
+GROUP BY
+    post.ID
+ORDER BY
+    post.ID ASC;
+
 -- Listar todos los usuarios que hayan publicado un post en Junio.
+
+SELECT
+    email
+FROM
+    usuario
+    JOIN post ON usuario.ID = post.usuario_id
+WHERE
+    to_char(fecha, 'MM') = '06';
